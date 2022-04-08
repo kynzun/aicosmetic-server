abc4ebe451bed5cec8d6a860ff614bb2adae870e
mongus <kjyanus@gmail.com>
2022-04-09 03:11:31 +0900

    jar setting

---
+++ src/main/java/com/kj/web/AICosmetic/domain/Product.java
@@ -0,0 +1,22 @@
+package com.kj.web.AICosmetic.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter@Setter
+public class Product {
+    @Id
+    @GeneratedValue
+    @Column(name = "product_id")
+    private Long id;
+
+    private String name;
+
+
+}
